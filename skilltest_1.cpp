a2ac505c7bb1a336e921629280751e1c17bd3535
junheartkenneth <47804370+junheartkenneth@users.noreply.github.com>
2019-03-13 13:05:30 +0800

    Add files via upload

---
+++ skilltest_1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+
+int pay
+int hour
+double tax
+tax = (pay * hour) * 0.14
+
+cout << "Enter Pay Rate per Hour"
+cin >> pay
+cout << "Enter Total Working Hours per Week"
+cin >> hour
+cout << "Gross Income:" << pay * hour << endl;
+cout << "Net Income:" << (pay * hour) - tax << endl;
+cout << "Clothes and Other Accessories:" << ((pay * hour) - tax) * 0.10) << endl;
+cout << "School Supplies:" << (((pay * hour) - tax) - (((pay * hour) - tax) * 0.10)) * 0.01 << endl;
+cout << "saving bonds:" << (((pay * hour) - tax) (((pay * hour) - tax) - (((pay * hour) - tax) * 0.10)) * 0.01) * 0.25 << endl;
+cout << "spenditure of parents:" << ((((pay * hour) - tax) (((pay * hour) - tax) - (((pay * hour) - tax) * 0.10)) * 0.01) * 0.25) * 0.50 << endl;
+
+_getch();
+return 0;
+}
+
+
+
+
